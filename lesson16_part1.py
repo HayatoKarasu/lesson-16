37849557b9728f4a5aaef3c754c906c8ce805b9f
HayatoKarasu <138600849+HayatoKarasu@users.noreply.github.com>
2023-08-02 19:42:14 +0700

    Add files via upload

---
+++ lesson16_part1.py
@@ -0,0 +1,50 @@
+﻿class Cassa(object):
+    kol = 1000
+    
+    def top_up(self):
+        x = int(input("Введите сумму: "))
+        dey.kol += x
+        print(dey.kol)
+        return
+        
+    def count_1000(self):
+        a = dey.kol
+        a //= 1000
+        print("В кассе: ", a, "тыс. купюр")
+        
+        
+    def take_away(self):
+        x = int(input("Введите сумму: "))
+        dey.kol -= x
+        print(dey.kol)
+        return
+        
+        
+dey = Cassa()
+        
+while True:
+    print("МЕНЮ:")
+    print("1. Пополнить кассу")
+    print("2. Сколько 1000 купюр в кассе")
+    print("3. Изъять из кассы")
+    print("4. Выход")
+    
+    choice = input("Выберите действие: ")
+    
+    if choice == '1':
+        print("Вы выбрали пополнить кассу")
+        dey.top_up()
+        
+    elif choice == '2':
+        print("Вы выбрали сколько 1000 купюр")
+        dey.count_1000()
+        
+    elif choice == '3':
+        print("Вы выбрали изъять из кассы")
+        dey.take_away()
+         
+    elif choice == '4':
+        break
+        
+    else:
+        print("Неправильный выбор.")
---
+++ lesson16_part2.py
@@ -0,0 +1,59 @@
+﻿class Turtle(object):
+    
+    def __init__(self, x, y, s):
+        self.x = x
+        self.y = y
+        self.s = s
+    
+    def go_up(self):
+        self.y += self.s
+        return self.y
+        
+    
+    def go_down(self):
+        self.y -= self.s
+        return self.y
+    
+    
+    def go_left(self):
+        self.x -= self.s
+        return self.x
+        
+        
+    def go_right(self):
+        self.x += self.s
+        return self.x
+    
+    
+    def evolve(self):
+        self.s += 1
+        return self.s
+        
+        
+    def degrade(self):
+        if self.s <= 0:
+            print("Ошибка!")
+        else:
+            self.s -= 1
+            return self.s
+            
+            
+    def count_moves(self, x2, y2):
+        return self.x - x2 // self.s, self.y - y2 // self.s
+             
+                
+dvig = Turtle(14, 5, 38)
+
+print(dvig.go_up())
+
+print(dvig.go_down())
+
+print(dvig.go_left())
+
+print(dvig.go_right())
+
+print(dvig.evolve())
+
+print(dvig.degrade())
+
+print(dvig.count_moves(32, 52))
